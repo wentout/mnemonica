763b1313823e2e6a471f249b5ce85cbaf60c30d9
went.out <went.out@gmail.com>
2019-12-10 23:43:17 +0300

    rules fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,69 +1,67 @@
 module.exports = {
-	'env': {
-		'node': true,
-		'es6': true,
-		'mocha': true,
+	env: {
+		node: true,
+		es6: true,
+		mocha: true,
 	},
-	'extends': 'eslint:recommended',
-	'parserOptions': {
-		'ecmaVersion': 2018
+	extends: ['eslint:recommended'],
+	parserOptions: {
+		ecmaVersion: 2018,
 	},
-	'plugins' : [
-		'mocha',
-		'eslint-plugin-no-arrow-this'
-	],
-	'rules': {
-		'linebreak-style': [
-			'error',
-			'unix'
+	// plugins: ['mocha', 'plugin:prettier/recommended', 'eslint-plugin-no-arrow-this'],
+	plugins: ['mocha', 'eslint-plugin-no-arrow-this'],
+	rules: {
+		'key-spacing': [
+			'warn',
+			{
+				beforeColon: true,
+				afterColon: true,
+				align: 'colon',
+			},
 		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
 		'no-unused-vars': 'warn',
-		'no-shadow' : [
-			'error', {
-				'builtinGlobals' : true,
-				'hoist' : 'all',
-				'allow' : []
-			}
+		'no-shadow': [
+			'error',
+			{
+				builtinGlobals: true,
+				hoist: 'all',
+				allow: [],
+			},
 		],
 		'prefer-template': 'warn',
 		'prefer-spread': 'warn',
 		'no-useless-concat': 'warn',
 		'prefer-rest-params': 'warn',
 		'prefer-destructuring': 'warn',
-		'no-useless-computed-key' : 'warn',
-		'no-useless-constructor' : 'warn',
-		'no-useless-rename' : 'warn',
-		'no-this-before-super' : 'warn',
-		'no-new-symbol' : 'warn',
-		'no-duplicate-imports' : 'warn',
-		'no-confusing-arrow' : 'warn',
-		'no-multi-assign' : 'warn',
-		'no-lonely-if' : 'warn',
-		'newline-per-chained-call' : 'warn',
-		'new-cap' : 'warn',
-		'func-name-matching' : 'error',
+		'no-useless-computed-key': 'warn',
+		'no-useless-constructor': 'warn',
+		'no-useless-rename': 'warn',
+		'no-this-before-super': 'warn',
+		'no-new-symbol': 'warn',
+		'no-duplicate-imports': 'warn',
+		'no-confusing-arrow': 'warn',
+		'no-multi-assign': 'warn',
+		'no-lonely-if': 'warn',
+		'newline-per-chained-call': 'warn',
+		'new-cap': 'warn',
+		'func-name-matching': 'error',
 		// 'consistent-this' : 'error',
-		'line-comment-position' : [
-			'warn', {
-				position : 'above'
-			}
+		'line-comment-position': [
+			'warn',
+			{
+				position: 'above',
+			},
 		],
-		'quotes': [
-			'error', 'single'
-		],
+		quotes: ['error', 'single'],
 		'no-arrow-this/no-arrow-this': [
-			'error', {
-				onlyGlobals : true
-			}
+			'error',
+			{
+				onlyGlobals: true,
+			},
 		],
-		'yoda': 'warn'
-	}
+		yoda: 'warn',
+	},
 };
