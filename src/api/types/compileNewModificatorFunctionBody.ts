785a0ec9a51d3dce92a0de35f669f94d97203522
went.out <went.out@gmail.com>
2020-06-18 15:45:47 +0300

    instance methods error handler additions

--- src/api/types/compileNewModificatorFunctionBody.ts
+++ src/api/types/compileNewModificatorFunctionBody.ts
@@ -3,17 +3,20 @@
 const compileNewModificatorFunctionBody = function ( FunctionName: string, asClass: boolean = false ) {
 
 	const modString = asClass ?
+
 		`class ${FunctionName} extends ConstructHandler {
-		constructor(...args) {
-			const answer = super(...args);
-			return CreationHandler.call(this, answer);
-		}
-	}`
+			constructor(...args) {
+				const answer = super(...args);
+				return CreationHandler.call(this, answer);
+			}
+		}`
+
 		:
+
 		`const ${FunctionName} = function (...args) {
-		const answer = ConstructHandler.call(this, ...args);
-		return CreationHandler.call(this, answer);
-	};`;
+			const answer = ConstructHandler.call(this, ...args);
+			return CreationHandler.call(this, answer);
+		};`;
 
 	return new Function( 'ConstructHandler', 'CreationHandler', 'SymbolConstructorName',
 		`return function () {
