9b9fa9647157e7dae9e75cb2e41861785a233101
wentout <went.out@gmail.com>
2022-08-04 19:25:18 +0300

    updates on naming conventions

--- build/api/types/compileNewModificatorFunctionBody.js
+++ build/api/types/compileNewModificatorFunctionBody.js
@@ -1,19 +1,20 @@
 'use strict';
 Object.defineProperty(exports, '__esModule', { value : true });
 const compileNewModificatorFunctionBody = function (FunctionName, asClass = false) {
+	const dt = `${Date.now()}_${`${Math.random()}`.split('.')[1]}`;
 	const modString = asClass ?
-		`class ${FunctionName} extends ConstructHandler {
+		`class ${FunctionName} extends ConstructHandler_${dt} {
 			constructor(...args) {
 				const answer = super(...args);
-				return CreationHandler.call(this, answer);
+				return CreationHandler_${dt}.call(this, answer);
 			}
 		}`
 		:
 		`const ${FunctionName} = function (...args) {
-			const answer = ConstructHandler.call(this, ...args);
-			return CreationHandler.call(this, answer);
+			const answer = ConstructHandler_${dt}.call(this, ...args);
+			return CreationHandler_${dt}.call(this, answer);
 		};`;
-	return new Function('ConstructHandler', 'CreationHandler', 'SymbolConstructorName', `return function () {
+	return new Function(`ConstructHandler_${dt}`, `CreationHandler_${dt}`, 'SymbolConstructorName', `return function () {
 
 			${modString}
 
--- src/api/types/compileNewModificatorFunctionBody.ts
+++ src/api/types/compileNewModificatorFunctionBody.ts
@@ -3,8 +3,10 @@
 /*
 
 // it is not that easy
-// constructor name diappears
-// frome the debugger and so on...
+// constructor name diappears when you look on 'this'
+// using Chrome Dev tools debugger for example
+// adding 'debugger;' keyword next line to 
+// 'const answer = ' see example down below
 
 // thought for console.log it is there
 
@@ -12,7 +14,7 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/name
 
 // the name of the function might be obfuscated during bundling
-// therefore it seems to be more correct to implement new Function
+// therefore it seems to be more correct to implement using 'new Function'
 
 // however, for better understanding of what is going on here
 // I'd like to provide solution with preliminary compiled functions
@@ -21,6 +23,7 @@ const getClassConstructor = ( ConstructHandler: any, CreationHandler: any, ) => 
 	return class extends ConstructHandler {
 		constructor ( ...args: any[] ) {
 			const answer = super( ...args );
+			debugger;
 			return CreationHandler.call( this, answer );
 		}
 	};
@@ -29,6 +32,7 @@ const getClassConstructor = ( ConstructHandler: any, CreationHandler: any, ) => 
 const getFunctionConstructor = ( ConstructHandler: any, CreationHandler: any, ) => {
 	return function ( this: any, ...args: any[] ) {
 		const answer = ConstructHandler.call( this, ...args );
+		debugger;
 		return CreationHandler.call( this, answer );
 	};
 };
@@ -60,24 +64,26 @@ const compileNewModificatorFunctionBody = function ( FunctionName: string, asCla
 */
 
 const compileNewModificatorFunctionBody = function ( FunctionName: string, asClass: boolean = false ) {
+	
+	const dt = `${Date.now()}_${`${Math.random()}`.split('.')[1]}`;
 
 	const modString = asClass ?
 
-		`class ${FunctionName} extends ConstructHandler {
+		`class ${FunctionName} extends ConstructHandler_${dt} {
 			constructor(...args) {
 				const answer = super(...args);
-				return CreationHandler.call(this, answer);
+				return CreationHandler_${dt}.call(this, answer);
 			}
 		}`
 
 		:
 
 		`const ${FunctionName} = function (...args) {
-			const answer = ConstructHandler.call(this, ...args);
-			return CreationHandler.call(this, answer);
+			const answer = ConstructHandler_${dt}.call(this, ...args);
+			return CreationHandler_${dt}.call(this, answer);
 		};`;
 
-	return new Function( 'ConstructHandler', 'CreationHandler', 'SymbolConstructorName',
+	return new Function( `ConstructHandler_${dt}`, `CreationHandler_${dt}`, 'SymbolConstructorName',
 		`return function () {
 
 			${modString}
